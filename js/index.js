ac5620a8c7cdf9eb411643a2f3f53d874620d159
damlaevcin <dmlaevcinn@gmail.com>
2023-03-05 14:52:37 +0300

    .

--- js/index.js
+++ js/index.js
@@ -10,5 +10,20 @@ for (var x = 1; x < 4; x++) {
 var vegetable = ["ıspanak", "kara lahana", "havuc"];
 
 vegetable.forEach(function (value, key) {
-    alert("value:" + value + " key" + key);
+    //alert("value:" + value + " key" + key);
 })
+
+var images = [
+    "https://picsum.photos/id/0/367/267",
+    "https://picsum.photos/id/1/367/267",
+    "https://picsum.photos/id/2/367/267"
+];
+
+
+var allImages = "";
+images.forEach(function (value, key) {
+    allImages = allImages + "<img src='"+ value + " '>"
+});
+
+$(".images").html(allImages)
+
