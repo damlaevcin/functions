7c4ee2e2d61357bde9145c4269756e86338e943d
damlaevcin <dmlaevcinn@gmail.com>
2023-03-05 14:25:35 +0300

    .

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
-function meyve(x){
-    var meyveler=["portakal","kiraz", "çilek","muz"];
+function meyve(x) {
+    var meyveler = ["portakal", "kiraz", "çilek", "muz"];
     return meyveler[x];
 }
 
 for (var x = 1; x < 4; x++) {
-    alert(meyve(x));
+    //alert(meyve(x));
 }
 
+var vegetable = ["ıspanak", "kara lahana", "havuc"];
+
+vegetable.forEach(function (value, key) {
+    alert("value:" + value + " key" + key);
+})
